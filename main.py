32e602ee1ac9a40362c66ec7c0264cefbf460a88
Maxim Ivliev <muss123balakovo@gmail.com>
2023-06-08 22:32:47 +0300

    Added final calculation function + proper call in main + comment all test calls

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-
-
-def parse(input):
+def parse(expression):
     res = []
     number = ''
 
-    for i in input:
+    for i in expression:
         if i in "1234567890.":
             number += i
         elif number:
@@ -13,42 +11,77 @@ def parse(input):
         if i in "+-*/()":
             res.append(i)
     if number:
-        res.append(number)
+        res.append(float(number))
 
     return res
 
 
-def sortExp(input):
+def sortexpression(parsedexpression):
     res = []
-    curstack = []
-    for i in input:
+    curexp = []
+    for i in parsedexpression:
         if str(i) in "+-*/":
             # priority1 = "+-*/()".find(i)
             # priority2 = "+-*/()".find(curstack[-1])
             # while curstack and curstack[-1] != '(' and priority1 <= priority2:
-            while curstack and curstack[-1] != '(' and "+-*/".find(i) <= "+-*/".find(curstack[-1]):
-                res.append(curstack.pop())
-            curstack.append(i)
+            while curexp and curexp[-1] != '(' and "+-*/".find(i) <= "+-*/".find(curexp[-1]):
+                res.append(curexp.pop())
+            curexp.append(i)
         elif i == ')':
-            while curstack:
-                last = curstack.pop()
+            while curexp:
+                last = curexp.pop()
                 if last == '(':
                     break
                 res.append(last)
         elif i == '(':
-            curstack.append(i)
+            curexp.append(i)
         else:
             res.append(i)
-    while curstack:
-        res.append(curstack.pop())
+    while curexp:
+        res.append(curexp.pop())
     return res
 
 
+def calculatepolish(sortedexpression):
+    numbers = []
+    for i in sortedexpression:
+        if str(i) in "+-*/":
+            second = numbers.pop()
+            first = numbers.pop()
+            res = 0
+
+            # match str(i):
+            #     case '+':
+            #         res = first + second
+            #     case '-':
+            #         res = first - second
+            #     case '*':
+            #         res = first * second
+            #     case '/':
+            #         res = first / second
+
+            if str(i) == '+':
+                res = first + second
+            elif str(i) == '-':
+                res = first - second
+            elif str(i) == '*':
+                res = first * second
+            elif str(i) == '/':
+                res = first / second
+
+            numbers.append(res)
+        else:
+            numbers.append(i)
+    return numbers[0]
+
+
 if __name__ == '__main__':
-    exp = parse(str(input()))
-    print(exp)
-    exp = sortExp(exp)
-    print(exp)
-#    print(exp[-1])
+    print(calculatepolish(sortexpression(parse(str(input())))))
+#     exp = parse(str(input()))
+#     print(exp)
+#     exp = sortexpression(exp)
+#     print(exp)
+#     exp = calculatepolish(exp)
+#     print(exp)
 # 12+(16+3*4-1)
 # (1+2)*4-1
